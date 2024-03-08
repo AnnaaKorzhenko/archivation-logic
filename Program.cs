59b8cdf8a12d651da50bf20f0415afcc001b2260
Anna Korzhenko <akorzhenko@kse.org.ua>
2024-03-08 19:06:02 +0200

    working plan

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// go through the file and create a dict with characters as keys and frequency of appearing as values
+
+// build a tree with those nodes of key-value frequencies and links to r-child and l-child
+
+// go through the tree and create a table with code for each character in the tree
+
+// translate the text into this code
+
+// add a decoding algorithm with a table in the end like %A:1%B:100%C:1000%D:100000
